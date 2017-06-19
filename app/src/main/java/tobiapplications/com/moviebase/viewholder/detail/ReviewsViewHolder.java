66faf6a9d738bf977645f955062ddc4deb0b678a
Tobias <tobiashehrlein@googlemail.com>
2017-06-19 22:00:00 +0200

    updated review holder to only show one review at first and show next on textview click

--- app/src/main/java/tobiapplications/com/moviebase/viewholder/detail/ReviewsViewHolder.java
+++ app/src/main/java/tobiapplications/com/moviebase/viewholder/detail/ReviewsViewHolder.java
@@ -1,7 +1,6 @@
 package tobiapplications.com.moviebase.viewholder.detail;
 
 import android.content.Context;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -19,30 +18,64 @@ import tobiapplications.com.moviebase.ui.general_views.ReviewItem;
  * Created by Tobias on 16.06.2017.
  */
 
-public class ReviewsViewHolder extends RecyclerView.ViewHolder {
+public class ReviewsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
     private Context context;
     private LinearLayout reviewWrapper;
+    private TextView showNextReview;
+    private int shownReviews = 1;
+    private ArrayList<ReviewItem> inflatedReviewItems;
 
     public ReviewsViewHolder(View itemView, Context context) {
         super(itemView);
-        this.reviewWrapper = (LinearLayout) itemView.findViewById(R.id.reviews_layout);
         this.context = context;
+        this.reviewWrapper = (LinearLayout) itemView.findViewById(R.id.reviews_layout);
+        this.showNextReview = (TextView) itemView.findViewById(R.id.show_next_review);
+        showNextReview.setOnClickListener(this);
     }
 
     public void setReviews(ReviewResponse reviewResponse) {
         ArrayList<Review> reviews = reviewResponse.getReviews();
+        inflatedReviewItems = new ArrayList<>();
 
         for (int i = 0; i < reviews.size(); i++) {
             ReviewItem item = new ReviewItem(context);
             item.setReviewText(reviews.get(i).getAuthor(), reviews.get(i).getContent());
-
             reviewWrapper.addView(item);
 
-            if (i + 1 < reviews.size()) {
-                DividerView divider = new DividerView(context);
-                reviewWrapper.addView(divider);
+            if (i > 0) {
+                item.setVisibility(View.GONE);
             }
+            inflatedReviewItems.add(item);
         }
+
+        hideLoadMoreTextViewIfLastReviewIsShown();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.show_next_review) {
+            addDivider();
+            inflatedReviewItems.get(shownReviews).setVisibility(View.VISIBLE);
+            shownReviews++;
+            hideLoadMoreTextViewIfLastReviewIsShown();
+        }
+    }
+
+    private void hideLoadMoreTextViewIfLastReviewIsShown() {
+        if (shownReviews == inflatedReviewItems.size()) {
+            showNextReview.setVisibility(View.GONE);
+        }
+    }
+
+    private void addDivider() {
+        if (shownReviews < inflatedReviewItems.size()) {
+            DividerView divider = new DividerView(context);
+            reviewWrapper.addView(divider, getDividerPosition());
+        }
+    }
+
+    public int getDividerPosition() {
+        return shownReviews * 2 - 1;
     }
 }
