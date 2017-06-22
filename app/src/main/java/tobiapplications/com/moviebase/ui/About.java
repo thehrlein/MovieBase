d0f1377687da24d685a895276b142f18bf719d18
Tobias <tobiashehrlein@googlemail.com>
2017-06-22 22:39:37 +0200

    added send email in about screen

--- app/src/main/java/tobiapplications/com/moviebase/ui/About.java
+++ app/src/main/java/tobiapplications/com/moviebase/ui/About.java
@@ -1,5 +1,6 @@
 package tobiapplications.com.moviebase.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -19,12 +20,22 @@ public class About extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener((View v) -> doFloatingAction());
+        fab.setOnClickListener((View v) -> sendEmail());
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    private void doFloatingAction() {
+    private void sendEmail()
+    {
+        String text = getString(R.string.hello_tobias);
+        String email = getString(R.string.tobiapplications);
 
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("message/rfc822");
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.app_name));
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[] {email});
+        Intent mailer = Intent.createChooser(intent, null);
+        startActivity(mailer);
     }
 
 }
