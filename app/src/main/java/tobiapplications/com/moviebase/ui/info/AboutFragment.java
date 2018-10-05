dece8b72efc869926e99b72c4f97891c8d08084e
Tobias Hehrlein <tobiashehrlein@googlemail.com>
2018-10-05 20:58:52 +0200

    add privacy policy

--- app/src/main/java/tobiapplications/com/moviebase/ui/info/AboutFragment.java
+++ app/src/main/java/tobiapplications/com/moviebase/ui/info/AboutFragment.java
@@ -1,6 +1,7 @@
 package tobiapplications.com.moviebase.ui.info;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -51,6 +52,13 @@ public class AboutFragment extends Fragment implements AboutContract.View {
     private void initialize() {
         getActivity().setTitle(R.string.about);
         bind.fab.setOnClickListener(v -> presenter.fabButtonClicked());
+        bind.privacyPolicy.setOnClickListener(v -> openPrivacyPolicy());
+
+    }
+
+    private void openPrivacyPolicy() {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://termsfeed.com/privacy-policy/383486908c603a4b2de14086ae9dd1d0"));
+        startActivity(browserIntent);
     }
 
     @Override
